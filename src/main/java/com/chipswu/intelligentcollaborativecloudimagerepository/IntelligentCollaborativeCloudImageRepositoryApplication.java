68566534170273344573ba53529fb41bae283c4e
WuJiaJun <wujiajun303@foxmail.com>
2026-05-04 21:42:05 +0800

    完成部分功能开发

--- src/main/java/com/chipswu/intelligentcollaborativecloudimagerepository/IntelligentCollaborativeCloudImageRepositoryApplication.java
+++ src/main/java/com/chipswu/intelligentcollaborativecloudimagerepository/IntelligentCollaborativeCloudImageRepositoryApplication.java
@@ -34,6 +34,7 @@ public class IntelligentCollaborativeCloudImageRepositoryApplication {
         log.info("地址：\thttp://127.0.0.1:{}{}",
                 env.getProperty("server.port"),
                 env.getProperty("server.servlet.context-path"));
+        log.info("");
     }
 
 }
